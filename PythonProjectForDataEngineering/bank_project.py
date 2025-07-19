3f958c138b65a2df4dcc372344e8ca994bc8ebc9
Ankur <105734576+ankurkagithub@users.noreply.github.com>
2025-07-19 12:12:38 +0100

    feat(extract): Data extraction feature added

--- PythonProjectForDataEngineering/bank_project.py
+++ PythonProjectForDataEngineering/bank_project.py
@@ -15,8 +15,52 @@ log_file = 'code_log.txt'
 
 
 def log_info(message):
+    ''' This function logs the messages to a log file with a timestamp.
+    The log file is created if it does not exist.
+    '''
     file = 'code_log.txt'
     timeStamp_format = '%Y-%m-%d %H:%M:%S'
     timeStamp = datetime.now().strftime(timestamp_format)
     with open(file, 'a+') as f:
         f.write(f'{timeStamp} - {message}\n')
+
+def extract(url, table_attribs):
+    '''
+    Scraping data from provided URL, parsing it and converting it into
+    a DataFrame.
+    '''
+    response = requests.get(url)
+    log_info(f'URL Fetched: status code is {response.status_code}')
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        table = soup.find_all('table')[0]
+        rows = table.find_all('tr')
+        rows = rows[1:]
+        data = {}
+        # data['rank'] = []
+        data[table_attribs[0]] = []
+        data[table_attribs[1]] = []
+        for row in rows:
+            # rank = int(row.find_all('td')[0].text.strip())
+            bank_name = row.find_all('td')[1].find_all('a')[-1].text
+            market_cap = float(row.find_all('td')[2].text.strip())
+            # data['rank'].append(rank)
+            data[table_attribs[0]].append(bank_name)
+            data[table_attribs[1]].append(market_cap)
+        df = pd.DataFrame(data)
+        log_info('Extraction: data extracted and loaded into a DataFrame')
+        return df
+
+
+def transform(df, csv_path):
+    ''' This function accesses the CSV file for exchange rate
+    information, and adds three columns to the data frame, each
+    containing the transformed version of Market Cap column to
+    respective currencies
+    '''
+    ex_df = pd.read_csv(exchange_rates, index_col='Currency') # Converting currency into a DF
+    df['market_cap(EUR_billion)'] = df['market_cap(USD_billion)'] * ex_df.loc['EUR','Rate']
+    df['market_cap(GBP_billion)'] = df['market_cap(USD_billion)'] * ex_df.loc['GBP','Rate']
+    df['market_cap(INR_billion)'] = df['market_cap(USD_billion)'] * ex_df.loc['INR','Rate']
+    log_info('Transformation: New currency added for EUR, GBP and INR')
+    return df
