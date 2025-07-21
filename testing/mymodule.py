3fde8242ff816067675973a23d69273ac24fa99a
Ankur <105734576+ankurkagithub@users.noreply.github.com>
2025-07-21 14:39:48 +0100

    test: Writted basic test for documentation and for future access

---
+++ testing/mymodule.py
@@ -0,0 +1,21 @@
+'''
+Function squares or double a given number.
+User can specify which function to use.
+'''
+def square(number):
+    """
+    This function returns the square of a given number
+    """
+    return number ** 2
+
+def double(number):
+    """
+    This function returns twice the value of a given number
+    """
+    return number * 2
+
+def add(a,b):
+    """
+    This function returns the sum of the given numbers.
+    """
+    return a + b
